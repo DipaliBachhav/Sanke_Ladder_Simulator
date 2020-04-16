309715cfb223648065f8600598fb2bf6a8d171dc
DipaliBachhav <dipa2718@gmail.com>
2020-04-16 14:15:51 +0530

    Added Option Checks

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -2,5 +2,27 @@
 echo "Snake And Ladder Simulatior..."
 
 startPosition=0
+position=0
+
+NO_PLAY=0
+LADDER=1
+SANKE=2
+PLAYER1=0
+
 dice=$((1+RANDOM%6))
 
+read -p "Enter The Choice :=" choice
+case $choice in
+	$NO_PLAY)
+		startPosition=$startPosition
+		;;
+
+	$LADDER)
+		startPosition=$(($startPosition+$dice))
+		;;
+
+	$SNAKE)
+		startPosition=$(($startPosition-$dice))
+		;;
+esac
+echo $startPosition
